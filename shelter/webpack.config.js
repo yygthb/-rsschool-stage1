46ef0f57ee83229c5a500cb4eaa96ca4038b3919
Yura Ya <yy.ainu.dev@gmail.com>
2022-04-08 13:32:54 +0300

    init: shelter - multi page application

---
+++ shelter/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const pages = ['index', 'pets'];
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  devtool: isProd ? 'source-map' : false, // readable scripts in production
+  entry: {
+    index: './src/index.js',
+    pets: './src/pets.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [].concat(
+    pages.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          inject: 'body',
+          minify: {
+            collapseWhitespace: false,
+          },
+          template: `./src/${page}.html`,
+          filename: `./${page}.html`,
+          chunks: [page],
+        })
+    ),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })
+  ),
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: {} },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
