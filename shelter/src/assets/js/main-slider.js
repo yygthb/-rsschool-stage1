758d04f10d232de275f6d79751181805e35329df
Yura Ya <yy.ainu.dev@gmail.com>
2022-04-24 22:25:39 +0300

    fix: visible content on main page

--- shelter/src/assets/js/main-slider.js
+++ shelter/src/assets/js/main-slider.js
@@ -1,6 +1,6 @@
 import { Slider } from './components/Slider';
 
-const pets = document.querySelector('.rs-slider');
+const pets = document.querySelector('.pets__content');
 
 const slider = new Slider().init({
   sliderClassNames: 'pets__slider',
