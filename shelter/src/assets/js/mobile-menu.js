564e5fe82f93b48d64a2a4ae3cd779485d9ba2f2
Yura Ya <yy.ainu.dev@gmail.com>
2022-04-26 18:18:17 +0300

    fix: scrollable mobile menu; close-modal button with hover-effect

--- shelter/src/assets/js/mobile-menu.js
+++ shelter/src/assets/js/mobile-menu.js
@@ -4,18 +4,41 @@ const header = document.querySelector('.header-container');
 if (header) {
   const burger = header.querySelector('.burger');
   const menuContainer = header.querySelector('.header-nav');
+  let isBurgerOpened = false;
 
-  burger.onclick = () => {
-    menuContainer.classList.toggle('open');
-    burger.classList.toggle('open');
-    body.classList.toggle('lock');
-  };
+  burger.onclick = burgerOpen;
 
-  menuContainer.onclick = (e) => {
-    if (e.target.dataset.close === 'mobile-menu') {
+  menuContainer.onclick = burgerClose;
+
+  function burgerOpen(e) {
+    if (!isBurgerOpened) {
+      isBurgerOpened = true;
+      menuContainer.classList.add('open');
+      burger.classList.add('open');
+      body.classList.add('lock');
+
+      if (burger.closest('body.index')) {
+        const scrollToTop = window.scrollY;
+        burger.style.top = `${scrollToTop + 40}px`;
+      }
+    } else {
+      burgerClose(e);
+    }
+  }
+
+  function burgerClose(e) {
+    if (
+      e.target.dataset.close === 'mobile-menu' ||
+      e.currentTarget === burger
+    ) {
+      isBurgerOpened = false;
       menuContainer.classList.remove('open');
       burger.classList.remove('open');
       body.classList.remove('lock');
+
+      if (burger.closest('body.index')) {
+        burger.style.top = 0;
+      }
     }
-  };
+  }
 }
